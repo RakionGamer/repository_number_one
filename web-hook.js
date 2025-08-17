a8be909c76520b0eb3897ac1f4a4d7e70ac16c5b
RakionG (Ricardo Valera) <101957005+RakionGamer@users.noreply.github.com>
2025-08-16 20:19:13 -0400

    Update web-hook.js

--- web-hook.js
+++ web-hook.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 const token = process.env.TELEGRAM_BOT_TOKEN || '8053119867:AAG1vVTjrgAUlj0DTYeGKfYzDthLXmHNt2I';
 const bot = new TelegramBot(token);
 
-const webhookUrl = 'http://mp-tasas.vercel.app/api/bot';
+const webhookUrl = 'https://mp-tasas.vercel.app/api/bot';
 
 bot.setWebHook(webhookUrl)
   .then(() => console.log('✅ Webhook configurado en:', webhookUrl))
@@ -12,4 +12,5 @@ bot.setWebHook(webhookUrl)
 
 bot.getWebHookInfo()
   .then(info => console.log('ℹ️ Info del webhook:', info))
+
   .catch(console.error);
